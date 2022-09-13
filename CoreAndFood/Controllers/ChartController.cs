cba63fa54b34aaa83d3d0c6d71029cb35e38e697
Erdem Doğanay <68710746+erdemdoganayy@users.noreply.github.com>
2022-09-13 23:26:19 +0300

    Statistics Finished

--- CoreAndFood/Controllers/ChartController.cs
+++ CoreAndFood/Controllers/ChartController.cs
@@ -49,5 +49,39 @@ namespace CoreAndFood.Controllers
             }
             return chart;
         }
+
+        public IActionResult Statistics()
+        {
+            Context db = new Context();
+
+            var FruitID = db.Categories.Where(x => x.Name == "Fruits").Select(y => y.Id).FirstOrDefault();
+            var VegetablesID = db.Categories.Where(x => x.Name == "Vegetables").Select(y => y.Id).FirstOrDefault();
+
+            var TotalFood = db.Foods.Count();
+            ViewBag.FoodTotal = TotalFood;
+            var TotalCategory = db.Categories.Count();
+            ViewBag.CategoryTotal = TotalCategory;
+            var TotalFruits = db.Foods.Where(x => x.CategoryID == FruitID).Count();
+            ViewBag.FruitsTotal = TotalFruits;
+            var TotalVegetables = db.Foods.Where(x => x.CategoryID == VegetablesID).Count();
+            ViewBag.VegetablesTotal = TotalVegetables;
+            var TotalStocks = db.Foods.Sum(x => x.Stock);
+            ViewBag.StocksTotal = TotalStocks;
+            var TotalLegumes = db.Foods.Where(x => x.CategoryID == db.Categories.Where(y => y.Name == "Legumes").Select(z => z.Id).FirstOrDefault()).Count();
+            ViewBag.LegumesTotal = TotalLegumes;
+            var FoodStockMax = db.Foods.OrderByDescending(x => x.Stock).Select(y => y.Name).FirstOrDefault();
+            ViewBag.StockMax = FoodStockMax;
+            var FoodStockMin = db.Foods.OrderBy(x => x.Stock).Select(y => y.Name).FirstOrDefault();
+            ViewBag.StockMin = FoodStockMin;
+            var PriceAverage = db.Foods.Average(x => x.Price).ToString("0.00");
+            ViewBag.PriceAvg = PriceAverage;
+            var FruitsCount = db.Foods.Where(x => x.CategoryID == FruitID).Sum(y => y.Stock);
+            ViewBag.CountFruits = FruitsCount;
+            var VegetablesCount = db.Foods.Where(x => x.CategoryID == VegetablesID).Sum(y => y.Stock);
+            ViewBag.CountVegetables = VegetablesCount;
+            var MaxPrice = db.Foods.OrderByDescending(x => x.Price).Select(y => y.Name).FirstOrDefault();
+            ViewBag.HighPrice = MaxPrice;
+            return View();
+        }
     }
 }
